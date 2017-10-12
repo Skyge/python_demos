03337250f26c6bf3194cc037f6b453d6fdbf7ae7
skyge <18844062006@163.com>
2017-10-12 22:34:12 +0800

    Make pythonic

--- generate_code.py
+++ generate_code.py
@@ -1,46 +1,33 @@
 # -*- coding:utf-8 -*-
-# 2017/10/12 19:36
+# 2017/10/12 22:12
+# version 2.0
 '''
     generate a series of code
 '''
 
 import random
+import string
 
-all_code = []
 
-
-#生成26个大写的字母
-for x in range(65,91):
-    a = str(chr(x))  #生成对应的ASCII码对应的字符串
-    all_code.append(a)
-    
-#生成26个小写字母
-for x in range(97,123):
-    a = str(chr(x))  #生成对应的ASCII码
-    all_code.append(a)
-    
-#生成10个数字
-for x in range(10):
-    all_code.append(str(x))
-    
-
-#生成16位激活码
-def gen_code():
+def generate_code(length):
     '''
-        >>>gen_code()
-        
-        k7lKLQWLwo8KrGu6
-        
+       >>>gen_code()
+       
+       k7lKLQWLwo8KrGu6
+       
     '''
-    s=''
-    for x in range(16):
-        a = random.choice(all_code)
-        s += a
-    print(s)
+    chars = string.ascii_letters + string.digits
+    
+    return ''.join([random.choice(chars) for i in range(length)])
+
+
+def save_code(content):
+    with open('Result Code.txt', 'a') as file:
+        file.write(content + '\n')
 
 if __name__ == '__main__':
-    import os
-    for x in range(5):
-        gen_code()
-    os.system("pause")
+    for i in range(5):
+        content = generate_code(16)
+        print (content)
+        save_code(content)
 
