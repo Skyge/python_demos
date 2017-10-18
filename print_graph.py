ed2f3a500400142d66aeedbdb201215558a04871
skyge <18844062006@163.com>
2017-10-18 14:48:14 +0800

    Improvement

--- print_graph.py
+++ print_graph.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 #2017/10/18
-#version 1.1
+#version 2.0
 '''
     generate figure
 '''
@@ -16,7 +16,6 @@ def right_triangle(rows=5):
         * * * * *
         
     '''
-    print('打印直角三角形：')
     i = 0
     while i < rows:
         j = 0
@@ -26,11 +25,6 @@ def right_triangle(rows=5):
         print('')
         i += 1
  
-if __name__ == '__main__':
-    right_triangle(5)
-
-
-
 def equilateral_triangle(rows):
     '''
         >>>hollow_equilateral_triangle(4)
@@ -39,14 +33,8 @@ def equilateral_triangle(rows):
           *****
          *******
     '''
-    print('打印等腰三角形：')
     for i in range(rows):
         print(' '*(rows-i-1) + '*'* (2*i+1))
-
-if __name__ == '__main__':
-    equilateral_triangle(4)
-
-
 
 def hollow_equilateral_triangle(rows):
     '''
@@ -57,7 +45,6 @@ def hollow_equilateral_triangle(rows):
          *     *
         * * * * * 
     '''
-    print('打印空心等腰三角形：')
     for i in range(rows):
         if i == 0:
             print(' '*(rows-1) + '*')
@@ -66,9 +53,6 @@ def hollow_equilateral_triangle(rows):
             print(' '*(2*i-1) + '*')
         elif i == (rows-1):
             print('* '*i + '*')
-
-if __name__ == '__main__':
-    hollow_equilateral_triangle(9)
 
 def rectangle(length,width):
     '''
@@ -82,12 +66,38 @@ def rectangle(length,width):
             * * *
             * * *
     '''
-    print('打印矩形：')
     for i in range(width):
         print('* '* length)
 
-if __name__ == '__main__':
-    rectangle(4,3)
+def hollow_rectangle_01(length,width):
+    '''
+        >>>hollow_rectangle(5,4)
+            * * * * *
+            *       *
+            *       *
+            * * * * *
+        >>>hollow_rectangle(3,3)
+            * * *
+            *   *
+            * * *
+    '''
+    for i in range(width):
+        if i == 0 or i == width-1:
+            print('* '* length)
+        else:
+            print('*'+ ' '*((length-1)*2-1)+'*')
+
+def hollow_rectangle_02(length,width):
+    for i in range(width):
+        for k in range(length):
+            if i > 0 and i != width-1 :
+                if k == 0 or k == length-1:
+                    print('* ',end='')
+                else:
+                    print('  ',end='')
+            else:
+                print('* ',end='')
+        print('')
 
 def diamond(length):
     '''
@@ -102,19 +112,48 @@ def diamond(length):
            * *
             *
     '''
-    print('打印菱形：')
-    for i in range(length*2-1):
-        if i == 0:
-            print(' '*(length-1) + '*')
-        elif i <= (length-1):
-            print(' '*(length-i-1) + '*',end = '')
-            print(' '*(2*i-1) + '*')
-        elif i < (length-1)*2:
-            print(' '*(i-length+1) + '*',end = '')
-            print(' '*(2*(2*(length-1)-i)-1) + '*')
-        elif i == (length-1)*2:
-            print(' '*(length-1) + '*')
+    i=j=k= 1 #i用于控制图形行数，j用于控制空格的个数，k用于控制*的个数
+    #菱形的上半部分
+    for i in range(length):
+        for j in range(length - i):
+            print (' ',end='')   
+        for k in range(2 * i - 1):
+            if k == 0 or k == 2 * i - 2:
+                print ('*',end='')
+            else:
+                print (' ',end='')
+        print ('')
+    #菱形的下半部分
+    for i in range(length):
+        for j in range(i):
+            print (' ',end='')
+            
+        for k in range(2 * (length - i) - 1):
+            if k == 0 or k == 2 * (length - i) - 2:
+                print ('*',end='')
+            else:
+                print (' ',end='')
+        print ('')
         
 
 if __name__ == '__main__':
+    print('打印直角三角形：')
+    right_triangle(5)
+    
+    print('打印等腰三角形：')
+    equilateral_triangle(4)
+    
+    print('打印空心等腰三角形：')
+    hollow_equilateral_triangle(9)
+    
+    print('打印实心矩形：')
+    rectangle(4,3)
+    
+    print('打印空心矩形方法1：')
+    hollow_rectangle_01(5,4)
+    
+    print('打印空心矩形方法2：')
+    hollow_rectangle_02(5,4)
+    
+    print ('打印空心菱形，这里去掉if-else条件判断就是实心的')
     diamond(5)      
