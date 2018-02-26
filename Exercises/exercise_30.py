8207f30408dc5d4ff58803dbb2eb4589d7e094db
skyge <18844062006@163.com>
2018-02-26 22:04:10 +0800

    count and say sequence

--- Exercises/exercise_30.py
+++ Exercises/exercise_30.py
@@ -11,9 +11,7 @@ Question:
     
     You are given a target value to search.
     If found in the array return its index, otherwise return -1.
-
     You may assume no duplicate exists in the array.
-
 '''
 
 def search(nums, target):
@@ -45,5 +43,4 @@ if __name__ == "__main__":
     nums, target = [4,5,6,7,0,1,2], 4
     solution = search(nums, target)
     print(solution)
-    
-
+  
