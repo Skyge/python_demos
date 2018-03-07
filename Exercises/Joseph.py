0a51c618e7fa0b636592d92d6278cc8c6bf3b8e5
skyge <18844062006@163.com>
2018-03-07 22:33:24 +0800

    Update Joseph Circle

--- Exercises/Joseph.py
+++ Exercises/Joseph.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
-# 2018/03/06
-# version1.0
+# 2018/03/07
+# version1.1
 
-
+'''
 def findWho(num):
     """
     17人围成一圈，一次报数，报到3的倍数的人剔除队列，
@@ -25,3 +25,20 @@ def findWho(num):
 if __name__ == '__main__':
     num = list(range(1,18))
     print(findWho(num))
+'''
+def josephus(num, target):
+    """
+    问题：将编号为0～（N–1）这N个人进行圆形排列，
+    按顺时针从0开始报数，报到M–1的人退出圆形队列，
+    剩下的人继续从0开始报数，不断重复。
+    求最后出列者最初在圆形队列中的编号。
+    """
+    if num == 1:
+        return 0  
+    else:
+        return (josephus(num-1, target) + target) % num 
+
+if __name__ == '__main__':
+    num = int(input("请输入参与人数："))
+    target = int(input("请输入出列号码："))
+    print(josephus(num, target))
