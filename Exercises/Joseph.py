54027114f3d95e5cfe34b5fcd14d6e1cb6fe2077
skyge <18844062006@163.com>
2018-03-08 22:27:11 +0800

    Use a pythonic method to sovle the Josephus Circle.

--- Exercises/Joseph.py
+++ Exercises/Joseph.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
-# 2018/03/07
-# version1.1
+# 2018/03/08
+# version1.2
 
 '''
 def findWho(num):
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     num = list(range(1,18))
     print(findWho(num))
 '''
+'''
 def josephus(num, target):
     """
     问题：将编号为0～（N–1）这N个人进行圆形排列，
@@ -37,6 +38,21 @@ def josephus(num, target):
         return 0  
     else:
         return (josephus(num-1, target) + target) % num 
+'''
+from collections import deque
+
+def josephus(num, target):
+    """
+    一排人，编号从1开始到17.所有人按序围成一个圈，
+    从编号1开始，挨个报数，从1开始，
+    报到3则出局，然后再次从1开始报数，直到只剩下一个人，
+    请问最后留下的是编号几？
+    """
+    q = deque(range(1, num + 1))
+    while len(q) > 1:
+        q.rotate(1 - target)
+        q.popleft()
+    return q
 
 if __name__ == '__main__':
     num = int(input("请输入参与人数："))
